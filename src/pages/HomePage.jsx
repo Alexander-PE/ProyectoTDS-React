0072b18f056bafe84a8b60ce80f74e1f9c4e9a3e
AlexanderPerez <alexander.pereze02@gmail.com>
2023-02-22 11:35:15 -0500

    Update: HomePage layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,12 @@ import { UserContext } from '../UserContext'
 
 export const HomePage = () => {
     const { dataa } = useContext(UserContext);
-
+    console.log(dataa)
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-5'>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+
             {
-                
+                dataa.map(({ name }, index) => <Card key={index} name={name} />)
             }
 
         </div>
