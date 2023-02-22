2ea735c897c8ab4eed551b75f42df001951b8585
AlexanderPerez <alexander.pereze02@gmail.com>
2023-02-22 10:09:39 -0500

    Update: NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../UserContext'
+
+
+
+export const NavBar = () => { 
+
+  const { handleInputChange, searchText, handleSearch } = useContext(UserContext)
+  return (
+    <div className="flex flex-row items-center justify-between w-full p-5 shadow-xs">
+      <Link className="ml-8 text-xl text-white md:flex">
+        Desaparecidos
+      </Link>
+      <span className="flex justify-center w-full h-10 text-sm border border-gray-300 rounded-full cursor-pointer md:w-1/3 ml-6">
+        <form onSubmit={handleSearch}>
+          <input type="search" value={searchText} onChange={handleInputChange} name="serch" placeholder="Search" autoComplete='off' className="flex-grow px-4 text-sm rounded-l-full rounded-r-full focus:outline-none" />
+        </form>
+      </span>
+    </div>
+  )
+}
