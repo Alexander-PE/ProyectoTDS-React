fac815ee3a60c84749feabe3723fafce9d02fe55
AlexanderPerez <alexander.pereze02@gmail.com>
2023-02-22 11:34:50 -0500

    Update: navbar input field

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,11 +12,9 @@ export const NavBar = () => {
       <Link className="ml-8 text-xl text-white md:flex">
         Desaparecidos
       </Link>
-      <span className="flex justify-center w-full h-10 text-sm border border-gray-300 rounded-full cursor-pointer md:w-1/3 ml-6">
-        <form onSubmit={handleSearch}>
+        <form onSubmit={handleSearch} className="flex justify-center w-full h-10 text-sm border border-gray-300 rounded-full cursor-pointer md:w-1/3 ml-6">
           <input type="search" value={searchText} onChange={handleInputChange} name="serch" placeholder="Search" autoComplete='off' className="flex-grow px-4 text-sm rounded-l-full rounded-r-full focus:outline-none" />
         </form>
-      </span>
     </div>
   )
 }
